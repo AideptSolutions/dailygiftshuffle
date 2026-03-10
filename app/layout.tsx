5520b04e4cf5faa6406a1e1ad46b6526fd511a31
AJ <aj@aideptsolutions.com>
2026-03-09 21:24:42 -0700

    Add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,9 @@ export const metadata: Metadata = {
     site: '@thegiftshuffle',
   },
   icons: {
-    icon: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🎁</text></svg>",
+    icon: "/favicon.png",
+    shortcut: "/favicon.png",
+    apple: "/favicon.png",
   },
   metadataBase: new URL('https://thegiftshuffle.com'),
 };
